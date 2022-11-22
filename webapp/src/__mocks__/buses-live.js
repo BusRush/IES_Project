38a28f0a813964accd5a954450df4ee0889bb040
João Fonseca <joao.fonseca@ua.pt>
2022-11-22 16:42:12 +0000

    add live bus

--- webapp/src/__mocks__/buses-live.js
+++ webapp/src/__mocks__/buses-live.js
@@ -11,5 +11,17 @@ export const busesLive = [
       fuel: 40,
       passengers: 10,
     }
+  },
+  {
+    id: 'AVRBUS-B12133',
+    name: 'Buzão',
+    routeId: 'AVRBUS-L11',
+    metrics: {
+      timestamp: new Date(),
+      position: [40.63300, -8.65970],
+      speed: 100,
+      fuel: 35,
+      passengers: 20,
+    }
   }
 ];
