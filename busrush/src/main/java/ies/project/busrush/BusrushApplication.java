6e1cd10e31830fb9ea42d71dbf6b7df95a78f423
abutuc <andrebutuc@ua.pt>
2022-11-26 22:13:27 +0000

    spring boot project created

---
+++ busrush/src/main/java/ies/project/busrush/BusrushApplication.java
@@ -0,0 +1,13 @@
+package ies.project.busrush;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusrushApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusrushApplication.class, args);
+	}
+
+}
