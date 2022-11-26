4d85f72d9f97adfb86905809e4e40643c416951b
abutuc <andrebutuc@ua.pt>
2022-11-26 18:22:53 +0000

    fixed branch mix up

--- mobileapp/App.js
+++ mobileapp/App.js
@@ -2,7 +2,6 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import ChosenBusInformationScreen from "./screens/ChosenBusInformationScreen";
 import HomeScreen from "./screens/HomeScreen";
-import CustomRoutesScreen from "./screens/CustomRoutesScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -14,10 +13,6 @@ export default function App() {
         <Stack.Screen
           name="Chosen Bus Information"
           component={ChosenBusInformationScreen}
-        />
-        <Stack.Screen
-          name="Custom Routes Page"
-          component={CustomRoutesScreen}
         />
       </Stack.Navigator>
     </NavigationContainer>
