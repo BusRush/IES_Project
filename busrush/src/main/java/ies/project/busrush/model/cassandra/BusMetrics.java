b6e12ac841d9653c17fb25242d72c95b3d740ce0
icbaptista <ines.castro.batista@gmail.com>
2022-12-03 19:39:58 +0000

    IP-31 model for BusMetrics

--- busrush/src/main/java/ies/project/busrush/model/cassandra/BusMetrics.java
+++ busrush/src/main/java/ies/project/busrush/model/cassandra/BusMetrics.java
@@ -1,10 +1,14 @@
 package ies.project.busrush.model.cassandra;
 
+import java.sql.Date;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import org.springframework.data.cassandra.core.mapping.PrimaryKey;
+import org.springframework.data.cassandra.core.mapping.PrimaryKeyColumn;
 import org.springframework.data.cassandra.core.mapping.Table;
+import org.springframework.data.cassandra.core.cql.PrimaryKeyType;
+import org.springframework.data.cassandra.core.cql.Ordering;
 
 import javax.persistence.*;
 
@@ -15,16 +19,36 @@ import java.util.Collection;
 @Table("busMetrics")
 public class BusMetrics {
 
-    @PrimaryKey
-    private String id;
+    @PrimaryKeyColumn(
+        name = "bus_id", 
+        ordinal = 0, 
+        type = PrimaryKeyType.PARTITIONED)
+    private String bus_id;
 
-    @Column(name="registration", length = 8, nullable = false, unique = true)
-    private String registration;
+    @PrimaryKeyColumn(
+        name = "device_id", 
+        ordinal = 0, 
+        type = PrimaryKeyType.PARTITIONED)
+    private String device_id;
 
-    @Column(name="brand", nullable = false)
-    private String brand;
+    @PrimaryKeyColumn(
+        name = "date", 
+        ordinal = 0, 
+        type = PrimaryKeyType.PARTITIONED)
+    private Date date;
 
-    @Column(name="model", nullable = false)
-    private String model;
+    @PrimaryKeyColumn(
+        name = "timestamp", 
+        ordinal = 2, 
+        type = PrimaryKeyType.CLUSTERED, 
+        ordering = Ordering.ASCENDING)
+    private long timestamp;
+    
+    private int passengers;
+    private Double speed;
+    private Double fuel; 
+    private Double lat;
+    private Double lon;
+
 
 }
