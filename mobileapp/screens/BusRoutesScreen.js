c455cdc7fc01c772bb0235c5636c4c73e02b4345
abutuc <andrebutuc@ua.pt>
2022-12-05 18:19:29 +0000

    IP-35 searchable dropdown

--- mobileapp/screens/BusRoutesScreen.js
+++ mobileapp/screens/BusRoutesScreen.js
@@ -11,6 +11,8 @@ import { addDays } from "date-fns";
 import { Provider as PaperProvider } from "react-native-paper";
 import { Dimensions } from "react-native";
 import * as Location from "expo-location";
+import SearchableDropdown from "react-native-searchable-dropdown";
+
 datesWhitelist = [
   {
     start: new Date(),
@@ -22,6 +24,8 @@ function BusRoutes() {
   const [location, setLocation] = useState(null);
   const [errorMsg, setErrorMsg] = useState(null);
   const [closestBusStop, setClosestBusStop] = useState("Waiting...");
+  const [busStops, setBusStops] = useState([]);
+  const [nextBuses, setNextBuses] = useState([])
 
   useEffect(() => {
     (async () => {
@@ -50,6 +54,34 @@ function BusRoutes() {
     }
   };
 
+  const getAllStops = async () => {
+    try {
+      let busStops = [];
+      const response = await fetch("http://10.0.2.2:8080/api/stops");
+      const json = await response.json();
+      for (let i = 0; i < json.length; i++) {
+        busStops.push({ id: json[i].id, name: json[i].designation });
+      }
+      setBusStops(busStops);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+
+
+
+  getAllStops();
+  // console.log(busStops);
+
+  const getBusRoutes = async (stopOrigin) => {
+    try {
+      const response = await fetch("http://10.2.2.2:8080/api/schedules/next?origin_stop_id=" + stopOrigin.id + "&destination_stop_id=AVRBUS-S0005")
+      const json = await response.json();
+
+    }
+    console.log(stopOrigin);
+  };
   const navigation = useNavigation();
   const [selectedDate, setSelectedDate] = useState("");
   const [partida_search, setPartidaSearch] = useState("");
@@ -75,6 +107,48 @@ function BusRoutes() {
     <PaperProvider>
       <SafeAreaView className="pt-5">
         <View style={{ backgroundColor: "#245A8D" }}>
+          <SearchableDropdown
+            onTextChange={(text) => console.log(text)}
+            //On text change listner on the searchable input
+            onItemSelect={(item) => getBusRoutes(item)}
+            //onItemSelect called after the selection from the dropdown
+            containerStyle={{ padding: 5 }}
+            //suggestion container style
+            textInputStyle={{
+              //inserted text style
+              padding: 12,
+              borderWidth: 1,
+              borderColor: "#ccc",
+              backgroundColor: "#FAF7F6",
+            }}
+            itemStyle={{
+              //single dropdown item style
+              padding: 10,
+              marginTop: 2,
+              backgroundColor: "#FAF9F8",
+              borderColor: "#bbb",
+              borderWidth: 1,
+            }}
+            itemTextStyle={{
+              //text style of a single dropdown item
+              color: "#222",
+            }}
+            itemsContainerStyle={{
+              //items container style you can pass maxHeight
+              //to restrict the items dropdown hieght
+              maxHeight: "50%",
+            }}
+            items={busStops}
+            //mapping of item array
+            defaultIndex={2}
+            //default selected item index
+            placeholder="placeholder"
+            //place holder for the search input
+            resetValue={false}
+            //reset textInput Value with true and false state
+            underlineColorAndroid="transparent"
+            //To remove the underline from the android input
+          />
           <SearchBar
             placeholder="Inserir partida"
             containerStyle={{
