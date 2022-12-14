ec8dfbb791dfdf9ff4a9448138ac99116a47d5ce
icbaptista <ines.castro.batista@gmail.com>
2022-12-14 00:38:14 +0000

    reformulating connection to cassandra

--- busrush/src/main/java/ies/project/busrush/model/cassandra/BusMetrics.java
+++ busrush/src/main/java/ies/project/busrush/model/cassandra/BusMetrics.java
@@ -1,42 +1,100 @@
 package ies.project.busrush.model.cassandra;
 
-import java.util.Date;
-import lombok.Data;
-import lombok.AllArgsConstructor;
-
-import org.springframework.data.cassandra.core.mapping.PrimaryKey;
-import org.springframework.data.cassandra.core.mapping.Column;
-import org.springframework.data.cassandra.core.mapping.PrimaryKeyColumn;
-import org.springframework.data.cassandra.core.mapping.Table;
-import org.springframework.data.cassandra.core.cql.PrimaryKeyType;
-import org.springframework.data.cassandra.core.cql.Ordering;
-
 // {'device_id': 'AVRBUS-D0001', 'route_id': 'AVRBUS-L04', 'route_shift': '083000', 'timestamp': 1670868915, 'position': [40.63554147, -8.65516931], 'speed': 15.156, 'fuel': 98.878, 'passengers': 15}
 
-@Data
-@AllArgsConstructor
-@Table("bus_metrics")
 public class BusMetrics {
-
-    @PrimaryKeyColumn(
-        name = "bus_id", 
-        ordinal = 0, 
-        type = PrimaryKeyType.PARTITIONED)
     private String bus_id;
+    private Long timestamp;
+    private String route_id;
+    private String route_shift;
+    private String device_id;
+    private String[] position;
+    private Double speed;
+    private Double fuel;
+    private int passengers;
 
-    @PrimaryKeyColumn(
-        name = "timestamp", 
-        ordinal = 2, 
-        type = PrimaryKeyType.CLUSTERED, 
-        ordering = Ordering.ASCENDING)
-    private long timestamp;
+    public BusMetrics(String bus_id, Long timestamp, String route_id, String route_shift, String device_id, String[] position, Double speed, Double fuel, int passengers) {
+        this.bus_id = bus_id;
+        this.timestamp = timestamp;
+        this.route_id = route_id;
+        this.route_shift = route_shift;
+        this.device_id = device_id;
+        this.position = position;
+        this.speed = speed;
+        this.fuel = fuel;
+        this.passengers = passengers;
+    }
 
-    @Column("route_id") private String route_id;
-    @Column("route_shift") private String route_shift;
-    @Column("device_id") private String device_id;
-    @Column("position") private String[] position;
-    @Column("speed") private Double speed;
-    @Column("fuel") private Double fuel;
-    @Column("passengers") private int passengers;
-    
+    public Long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getBus_id() {
+        return bus_id;
+    }
+
+    public void setBus_id(String bus_id) {
+        this.bus_id = bus_id;
+    }
+
+    public String getRoute_id() {
+        return route_id;
+    }
+
+    public void setRoute_id(String route_id) {
+        this.route_id = route_id;
+    }
+
+    public String getRoute_shift() {
+        return route_shift;
+    }
+
+    public void setRoute_shift(String route_shift) {
+        this.route_shift = route_shift;
+    }
+
+    public String getDevice_id() {
+        return device_id;
+    }
+
+    public void setDevice_id(String device_id) {
+        this.device_id = device_id;
+    }
+
+    public String[] getPosition() {
+        return position;
+    }
+
+    public void setPosition(String[] position) {
+        this.position = position;
+    }
+
+    public Double getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(Double speed) {
+        this.speed = speed;
+    }
+
+    public Double getFuel() {
+        return fuel;
+    }
+
+    public void setFuel(Double fuel) {
+        this.fuel = fuel;
+    }
+
+    public int getPassengers() {
+        return passengers;
+    }
+
+    public void setPassengers(int passengers) {
+        this.passengers = passengers;
+    }
+
 }
--- busrush/src/main/java/ies/project/busrush/service/QueueService.java
+++ busrush/src/main/java/ies/project/busrush/service/QueueService.java
@@ -4,33 +4,41 @@ import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
-import org.json.JSONObject;
-import ies.project.busrush.repository.cassandra.BusMetricsRepository;
 
-import ies.project.busrush.model.cassandra.BusMetrics;
+import com.datastax.oss.driver.api.core.CqlSession;
+import com.datastax.oss.driver.api.core.cql.BoundStatement;
+import com.datastax.oss.driver.api.core.cql.PreparedStatement;
+
+import org.json.JSONObject;
+import ies.project.busrush.repository.BusRepository;
+import ies.project.busrush.repository.cassandra.BusMetricsRepository;
 
 
 @Service
 public class QueueService {
 
-    private final CassandraService cassandraService;
-    private final BusRushService busRushService;
-    private final BusMetricsRepository busMetricsRepository;
+    private BusRepository busRepository;
 
     @Autowired
-    public QueueService(CassandraService cassandraService, BusRushService busRushService, BusMetricsRepository busMetricsRepository) {
-        this.cassandraService = cassandraService; 
-        this.busRushService = busRushService;
-        this.busMetricsRepository = busMetricsRepository;
+    public QueueService(BusRepository busRepository) {
+        this.busRepository = busRepository;
     }
 
     @RabbitListener(queues = "devices")
     public void receive(@Payload String msg) {
 
+        // Message received from RabbitMQ queue
         // msg : {'device_id': 'AVRBUS-D0001', 'route_id': 'AVRBUS-L04', 'route_shift': '083000', 'timestamp': 1670868915, 'position': [40.63554147, -8.65516931], 'speed': 15.156, 'fuel': 98.878, 'passengers': 15}
 
+        CassandraConnector cassandra_connector = new CassandraConnector();
+        cassandra_connector.connect("localhost", 9042, "busrushdelays", "busrush");
+        CqlSession session = cassandra_connector.getSession();
+
+        BusMetricsRepository busMetricsRepository = new BusMetricsRepository(session);
+        busMetricsRepository.createTable("busrushdelays");        
+
         System.out.println("Received: " + msg); 
-            System.out.println("NEW"); 
+        System.out.println("NEW"); 
         
         JSONObject json = new JSONObject(msg);
 
@@ -47,17 +55,28 @@ public class QueueService {
         System.out.println("AQUIII");
 
         // Get field bus_id from MySQL
-        String bus_id = busRushService.getBusID(device_id);
+        String bus_id = busRepository.findBusIdByDeviceId(device_id);
 
         System.out.println("BUS ID: " + bus_id);
 
         // Create an instance of BusMetrics with some values
-        BusMetrics busMetrics = new BusMetrics(bus_id, timestamp, route_id, route_shift, device_id, position, speed, fuel, passengers);
-        busMetricsRepository.save(busMetrics); // Save the instance to Cassandra
+        //BusMetrics busMetrics = new BusMetrics(bus_id, timestamp, route_id, route_shift, device_id, position, speed, fuel, passengers);
+        //busMetricsRepository.save(busMetrics); // Save the instance to Cassandra
 
         // Send to Cassandra
-        cassandraService.insertMetrics(bus_id, timestamp, route_id, route_shift, device_id, position, speed, fuel, passengers);
+        // insertMetrics(session, bus_id, timestamp, route_id, route_shift, device_id, position, speed, fuel, passengers);
+
+        cassandra_connector.close();
         
+    }
+
+     // Query protected against SQL injection
+     public void insertMetrics(CqlSession session, String bus_id, Long timestamp, String route_id, String route_shift, String device_id, String[] position, Double speed, Double fuel, int passengers) {
+        // Insert the given message into the "messages" table in Cassandra
+        String cql = "INSERT INTO bus_metrics (bus_id, timestamp, route_id, route_shift, device_id, position, speed, fuel, passengers) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        PreparedStatement preparedStatement = session.prepare(cql);
+        BoundStatement boundStatement = preparedStatement.bind(bus_id, timestamp, route_id, route_shift, device_id, position, speed, fuel, passengers);
+        session.execute(boundStatement);
     }
 
 }
