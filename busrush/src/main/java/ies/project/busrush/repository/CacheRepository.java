436aa024a72b812e57f599c17068b26cb28d08fa
Diogo Paiva <diogopaiva21@ua.pt>
2022-12-26 21:10:10 +0000

    IP-47 Cache done

--- busrush/src/main/java/ies/project/busrush/repository/CacheRepository.java
+++ busrush/src/main/java/ies/project/busrush/repository/CacheRepository.java
@@ -1,8 +1,12 @@
 package ies.project.busrush.repository;
 
+import ies.project.busrush.dto.cache.NextScheduleDtoc;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Set;
 
 @Component
 public class CacheRepository {
@@ -15,5 +19,17 @@ public class CacheRepository {
 
     public Object get(String s) {
         return redisTemplate.opsForValue().get(s);
+    }
+
+    public Set<Object> getAll(String s) {
+        return redisTemplate.opsForSet().members(s);
+    }
+
+    public boolean exists(String s) {
+        return redisTemplate.hasKey(s);
+    }
+
+    public void add(String s, NextScheduleDtoc object) {
+        redisTemplate.opsForSet().add(s, object);
     }
 }
